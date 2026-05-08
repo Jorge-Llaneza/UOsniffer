8baf7d064f0bfee32a5c9605859c26e078eff18c
jorge <llanezarodriguezjorge@gmail.com>
2026-05-08 14:09:57 +0200

    feature: min exams taken ranking command option

--- src/ui/parser.rs
+++ src/ui/parser.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use crate::commands::{Command, CreateRankingOptions};
 
 pub struct ConsoleCommandParser {
@@ -12,11 +13,35 @@ impl ConsoleCommandParser {
     }
 
     pub fn parse_line(&self, line: &str) -> Command {
-       match line.trim() {
+        let command = first_word(line);
+       match command {
            "q" | "exit" => Command::ExitProgram,
-           "ranking" => Command::CreateRanking(CreateRankingOptions::with_min_exams_taken(3)),
+           "ranking" => self.parse_ranking_commands(line),
            "commands" => Command::ShowAllCommands,
-           _ => Command::UnmatchedCommand(String::from(line.trim())),
+           _ => Command::UnmatchedCommand(String::from(command)),
        }
+    }
+
+    fn parse_ranking_commands(&self, line: &str) -> Command {
+        let parts = line.split(' ');
+        let mut min_exams_taken = 0;
+        for option in parts {
+            if option.starts_with("--min-exams-taken=") {
+                if let Some(parts) = option.split_once('=') {
+                    if let Ok(number) = u32::from_str(parts.1.trim()){
+                        min_exams_taken = number;
+                    }
+                }
+            }
+        }
+        Command::CreateRanking(CreateRankingOptions::with_min_exams_taken(min_exams_taken))
+    }
+}
+
+fn first_word(line: &str) -> &str {
+    if let Some(parts) = line.trim().split_once(' ') {
+        parts.0.trim()
+    } else {
+        line.trim()
     }
 }
